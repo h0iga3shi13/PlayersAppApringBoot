58c8fe0acc22f11f80a1e7e6571ea2d47f65a8ae
UserName <easton3.h@gmail.com>
2020-07-24 09:04:06 +0900

    HomeControllerを追加 Home画面に”本文”、”募集者”、”開催日”、”種類”を表示する仕様に変更

--- src/main/java/com/higashi/players/controller/HomeController.java
+++ src/main/java/com/higashi/players/controller/HomeController.java
@@ -1,5 +1,30 @@
 package com.higashi.players.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.higashi.players.entity.BBS;
+import com.higashi.players.repository.BBSRepository;
+
+@Controller
 public class HomeController {
 
+	@Autowired
+	BBSRepository bbsRepository;
+
+	/*
+	 * HOME画面
+	 * BBSのタイトル、募集者、開催日、募集者、種類を表示
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+		List<BBS> homelist = bbsRepository.findAll();
+		model.addAttribute("bbslist", homelist);
+		return "home";
+	}
 }
