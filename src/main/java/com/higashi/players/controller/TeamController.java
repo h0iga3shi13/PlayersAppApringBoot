c9536915c66215b768945d07fea00ebfa21038eb
UserName <easton3.h@gmail.com>
2020-07-24 19:01:31 +0900

    BBSに書き込まれたチーム名が表示されるように変更

--- src/main/java/com/higashi/players/controller/TeamController.java
+++ src/main/java/com/higashi/players/controller/TeamController.java
@@ -1,5 +1,31 @@
 package com.higashi.players.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.higashi.players.entity.BBS;
+import com.higashi.players.repository.BBSRepository;
+
+@Controller
 public class TeamController {
+
+	@Autowired
+	BBSRepository bbsRepository;
+
+	/*
+	 * Team一覧
+	 * チーム名を表示
+	 */
+	@RequestMapping(value = "/team", method = RequestMethod.GET)
+	public String index(Model model) {
+		List<BBS> teamlist = bbsRepository.findAll();
+		model.addAttribute("bbslist", teamlist);
+		return "team";
+	}
 
 }
