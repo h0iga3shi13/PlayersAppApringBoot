fe57f531a1134d5a55708b5e36f47a7847d1493f
UserName <easton3.h@gmail.com>
2020-08-03 11:33:02 +0900

    UserTableRepositoryからログイン中のユーザーの情報を取得できるように変更

--- src/main/java/com/higashi/players/service/BBSService.java
+++ src/main/java/com/higashi/players/service/BBSService.java
@@ -1,6 +1,7 @@
 package com.higashi.players.service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -9,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.higashi.players.dto.BBSForm;
+import com.higashi.players.dto.BBSRecord;
 import com.higashi.players.entity.BBS;
 import com.higashi.players.repository.BBSRepository;
+import com.higashi.players.repository.UserTableRepository;
+import com.higashi.players.util.LoginState;
 
 @Service
 @Transactional(rollbackOn = Exception.class)
@@ -22,12 +26,25 @@ public class BBSService {
 	@Autowired
 	BBSRepository bbsRepository;
 
+	@Autowired
+	UserTableRepository userTableRepository;
+
 	/*
 	 * BBS情報 全検索
 	 * return 検索結果
 	 */
-	public List<BBS> searchAll() {
-		return bbsRepository.findAll();
+	public List<BBSRecord> searchAll() {
+
+		List<BBSRecord> bbsRecords = new ArrayList<BBSRecord>();
+		for (BBS bbs : bbsRepository.findAll()) {
+			String userName = bbs.getUserTable().getAdd().getName();
+			BBSRecord bbsRecord = new BBSRecord();
+			bbsRecord.setBbs(bbs);
+			bbsRecord.setUserName(userName);
+			bbsRecords.add(bbsRecord);
+		}
+		return bbsRecords;
+
 	}
 
 	/*
@@ -47,7 +64,8 @@ public class BBSService {
 	private BBS createBBS(BBSForm formRequest) {
 
 		BBS bbs = new BBS();
-		bbs.setName(formRequest.getName());
+
+		bbs.setUserTable(userTableRepository.findByEmail(LoginState.loginEmail()));
 		bbs.setTeam(formRequest.getTeam());
 		bbs.setEvent(formRequest.getEvent().atTime(0, 0, 0));
 		bbs.setDeadline(formRequest.getDeadline().atTime(0, 0, 0));
